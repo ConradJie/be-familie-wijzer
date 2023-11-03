75e75134f50b7d2188592ce53eedcbce77de64e2
conradjie <conrad.jie@gmail.com>
2023-11-03 12:26:13 +0100

    renamed methodes

---
+++ src/main/java/com/jie/befamiliewijzer/controllers/RelationController.java
@@ -0,0 +1,53 @@
+package com.jie.befamiliewijzer.controllers;
+
+import com.jie.befamiliewijzer.dtos.RelationDto;
+import com.jie.befamiliewijzer.dtos.RelationInputDto;
+import com.jie.befamiliewijzer.services.RelationService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class RelationController {
+    final private RelationService relationService;
+
+    public RelationController(RelationService relationService) {
+        this.relationService = relationService;
+    }
+
+    @GetMapping("/relations/{id}")
+    public ResponseEntity<RelationDto> getRelation(@PathVariable Integer id) {
+        return ResponseEntity.ok(relationService.getRelation(id));
+    }
+
+    @GetMapping("/relations")
+    public ResponseEntity<List<RelationDto>> getAllRelations() {
+        return ResponseEntity.ok(relationService.getAllRelations());
+    }
+
+    @PostMapping("/relations")
+    public ResponseEntity<Object> createRelation(@Valid @RequestBody RelationInputDto relationInputDto) {
+        RelationDto relationDto = relationService.createRelation(relationInputDto);
+        URI uri = URI.create(ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/" + relationDto.id).toUriString());
+        return ResponseEntity.created(uri).body(relationDto);
+    }
+
+    @PutMapping("/relations/{id}")
+    public ResponseEntity<Object> updateRelation(@PathVariable Integer id,
+                                                 @Valid @RequestBody RelationInputDto relationInputDto) {
+        RelationDto dto = relationService.updateRelation(id, relationInputDto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping("/relations/{id}")
+    public ResponseEntity<RelationDto> deleteRelation(@PathVariable Integer id) {
+        relationService.deleteRelation(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/jie/befamiliewijzer/dtos/RelationInputDto.java
@@ -0,0 +1,7 @@
+package com.jie.befamiliewijzer.dtos;
+
+public class RelationInputDto {
+    public Integer personId;
+    public Integer spouceId;
+    public Integer childId;
+}
