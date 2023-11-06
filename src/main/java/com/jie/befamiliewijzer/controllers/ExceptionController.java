de892305da2017ec84fb83ddb503e4655b4f41ff
conradjie <conrad.jie@gmail.com>
2023-11-06 21:58:43 +0100

    CrossOrigin

--- src/main/java/com/jie/befamiliewijzer/controllers/ExceptionController.java
+++ src/main/java/com/jie/befamiliewijzer/controllers/ExceptionController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
+@CrossOrigin
 @ControllerAdvice
 public class ExceptionController {
     @ExceptionHandler(ResourceNotFoundException.class)
