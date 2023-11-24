bfbd09f245d80effa602b32887f22365681a6719
conradjie <conrad.jie@gmail.com>
2023-11-24 15:44:56 +0100

    test person-, relation-controlloer

--- src/test/java/com/jie/befamiliewijzer/controllers/PersonControllerTest.java
+++ src/test/java/com/jie/befamiliewijzer/controllers/PersonControllerTest.java
@@ -1,24 +1,27 @@
 package com.jie.befamiliewijzer.controllers;
 
 import com.jie.befamiliewijzer.dtos.PersonDto;
+import com.jie.befamiliewijzer.dtos.PersonInputDto;
 import com.jie.befamiliewijzer.services.PersonService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.*;
 
 @WebMvcTest(PersonController.class)
 @ActiveProfiles("test")
@@ -37,23 +40,349 @@ class PersonControllerTest {
     void tearDown() {
     }
 
-//    @Disabled
-//    @Test
-//    void getCorrectPerson() throws Exception {
-//        PersonDto dto = new PersonDto();
-//        dto.id = 12;
-//        dto.givenNames = "John";
-//        dto.surname = "Doe";
-//        dto.sex = "M";
-//
-//        Mockito.when(personService.getPerson(anyInt())).thenReturn(dto);
-//        this.mockMvc
-//                .perform(MockMvcRequestBuilders.get("/persons/12"))
-//                .andDo(MockMvcResultHandlers.print())
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.givenNames",is("John")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.surname",is("Doe")))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.sex",is("M")));
-//
-//    }
+    @Test
+    void testGetPerson() throws Exception {
+        PersonDto dto = new PersonDto();
+        dto.id = 11;
+        dto.givenNames = "John";
+        dto.surname = "Doe";
+        dto.sex = "M";
+
+        Mockito.when(personService.getPerson(anyInt())).thenReturn(dto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/12"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sex", is("M")));
+
+    }
+
+    @Test
+    void testGetAllPersonsByName() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersons()).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testGetAllPersons() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersonsByName(anyString())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/namecontains/Doe"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testGetAllPersonsContainsGivenNamesSurname() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersonsContainsGivenNamesAndSurname(anyString(), anyString())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/contains?givenNames=J&surname=Doe"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void TestGetAllPersonsContainsGivenNames() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersonsContainsGivenNames(anyString())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/contains?givenNames=J"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testGetAllPersonsContainsSurname() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersonsContainsSurname(anyString())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/contains?surname=Doe"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testGetAllPersonsContains() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getAllPersons()).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/contains"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testGetAllChildrenFromPerson() throws Exception {
+        PersonDto johnny = new PersonDto();
+        johnny.id = 13;
+        johnny.givenNames = "Johnny";
+        johnny.surname = "Doe";
+        johnny.sex = "M";
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(johnny);
+
+        Mockito.when(personService.getAllChildrenFromPerson(anyInt())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/children/11"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(13)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("Johnny")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")));
+    }
+
+    @Test
+    void testGetParentrsFromPerson() throws Exception {
+        PersonDto john = new PersonDto();
+        john.id = 11;
+        john.givenNames = "John";
+        john.surname = "Doe";
+        john.sex = "M";
+
+        PersonDto jane = new PersonDto();
+        jane.id = 12;
+        jane.givenNames = "Jane";
+        jane.surname = "Doe";
+        jane.sex = "F";
+
+        List<PersonDto> dtos = new ArrayList<>();
+        dtos.add(john);
+        dtos.add(jane);
+
+        Mockito.when(personService.getParentsPerson(anyInt())).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/persons/parents/13"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].sex", is("M")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].givenNames", is("Jane")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].sex", is("F")));
+    }
+
+    @Test
+    void testCreatePerson() throws Exception {
+
+        //PersonInputDto
+        String jsonString = "{\n" +
+                "\"givenNames\": \"John\",\n" +
+                "\"surname\": \"Doe\",\n" +
+                "\"sex\": \"M\"\n" +
+                "}\n";
+
+        PersonDto personDto = new PersonDto();
+        personDto.id = 11;
+        personDto.givenNames = "John";
+        personDto.surname = "Doe";
+        personDto.sex = "M";
+
+        Mockito.when(personService.createPerson(any(PersonInputDto.class))).thenReturn(personDto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/persons")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sex", is("M")));
+    }
+
+    @Test
+    void testUpdatePerson() throws Exception {
+
+        //PersonInputDto
+        String jsonString = "{\n" +
+                "\"givenNames\": \"John\",\n" +
+                "\"surname\": \"Doe\",\n" +
+                "\"sex\": \"M\"\n" +
+                "}\n";
+
+        PersonDto personDto = new PersonDto();
+        personDto.id = 11;
+        personDto.givenNames = "John";
+        personDto.surname = "Doe";
+        personDto.sex = "M";
+
+        Mockito.when(personService.updatePerson(anyInt(), any(PersonInputDto.class))).thenReturn(personDto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .put("/persons/11")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.givenNames", is("John")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname", is("Doe")))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.sex", is("M")));
+    }
+
+    @Test
+    void testDeletePerson() throws Exception {
+
+        personService.deletePerson(anyInt());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.delete("/persons/11"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
 }
---
+++ src/test/java/com/jie/befamiliewijzer/controllers/RelationControllerTest.java
@@ -0,0 +1,224 @@
+package com.jie.befamiliewijzer.controllers;
+
+import com.jie.befamiliewijzer.dtos.*;
+import com.jie.befamiliewijzer.services.RelationService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.*;
+
+@WebMvcTest(RelationController.class)
+@ActiveProfiles("test")
+class RelationControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    RelationService relationService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testGetRelation() throws Exception {
+        RelationDto dto = new RelationDto();
+        dto.id = 10;
+        dto.personId = 11;
+        dto.spouseId = 12;
+
+        Mockito.when(relationService.getRelation(anyInt())).thenReturn(dto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/relations/10"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(10)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.personId", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.spouseId", is(12)));
+    }
+
+//    @Test
+//    void testGetRelationsFromPerson() {
+//        RelationSpouseDto dto = new RelationSpouseDto();
+//        dto.id = 10;
+//        dto.personId = 11;
+//        dto.spouseId = 12;
+//        dto.spouseGivenNames = "Jane";
+//        dto.spouseSurname = "Doe";
+//        dto.spouseSex = "F";
+//        List<RelationSpouseDto> dtos = new ArrayList<>();
+//        dtos.add(dto);
+//
+//        Mockito.when(relationService.getAllRelationsFromPersonId(anyInt())).thenReturn(dtos);
+//
+//        this.mockMvc
+//                .perform(MockMvcRequestBuilders.get("/relations/persons/11"))
+//                .andDo(MockMvcResultHandlers.print())
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(10)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].personId", is(11)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].spouseId", is(12)))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].spouseGivenNames", is("Jane")))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].spouseSurname", is("Doe")))
+//                .andExpect(MockMvcResultMatchers.jsonPath("$[0].spouseSex", is("F")));
+//    }
+
+    @Test
+    void testGetRelationByPersonIdAndSpouseId() throws Exception {
+        RelationDto dto = new RelationDto();
+        dto.id = 10;
+        dto.personId = 11;
+        dto.spouseId = 12;
+
+        Mockito.when(relationService.getRelationByPersonIdAndSpouseId(anyInt(), anyInt())).thenReturn(dto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/relations/persons/11/120"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(10)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.personId", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.spouseId", is(12)));
+    }
+
+    @Test
+    void testGetAllRelations() throws Exception {
+        RelationDto dto0 = new RelationDto();
+        dto0.id = 10;
+        dto0.personId = 11;
+        dto0.spouseId = 12;
+
+        RelationDto dto1 = new RelationDto();
+        dto1.id = 11;
+        dto1.personId = 13;
+        dto1.spouseId = 14;
+        List<RelationDto> dtos = new ArrayList<>();
+        dtos.add(dto0);
+        dtos.add(dto1);
+
+        Mockito.when(relationService.getAllRelations()).thenReturn(dtos);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.get("/relations"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id", is(10)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].personId", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].spouseId", is(12)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].personId", is(13)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].spouseId", is(14)));
+    }
+
+    @Test
+    void testCreateRelation() throws Exception {
+
+        //RelationInputDto
+        String jsonString = """
+                {
+                "personId" : 11,
+                "spouseId" : 12
+                }
+                """;
+
+        RelationDto relationDto = new RelationDto();
+        relationDto.id = 10;
+        relationDto.personId = 11;
+        relationDto.spouseId = 12;
+
+        Mockito.when(relationService.createRelation(any(RelationInputDto.class))).thenReturn(relationDto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/relations")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(10)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.personId", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.spouseId", is(12)));
+    }
+
+    @Test
+    void testUpdateRelation() throws Exception {
+
+        //RelationInputDto
+        String jsonString = """
+                {
+                "id" : 10,
+                "personId" : 11,
+                "spouseId" : 12
+                }
+                """;
+
+        RelationDto relationDto = new RelationDto();
+        relationDto.id = 10;
+        relationDto.personId = 11;
+        relationDto.spouseId = 12;
+
+        Mockito.when(relationService.updateRelation(anyInt(), any(RelationInputDto.class))).thenReturn(relationDto);
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .put("/relations/10")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonString)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", is(10)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.personId", is(11)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.spouseId", is(12)));
+    }
+
+    @Test
+    void testDeleteRelation() throws Exception {
+
+        relationService.deleteRelation(anyInt());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.delete("/relations/10"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testRemovePersonFromRelation() throws Exception {
+
+        relationService.removePersonFromRelation(anyInt(), anyInt());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.delete("/relations/10/12"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testDeleteRelationByPersonIdAndSpouseId() throws Exception {
+
+        relationService.deleteRelationByPersonIdAndSpouseId(anyInt(), anyInt());
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders.delete("/relations/persons/11/12"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+}
