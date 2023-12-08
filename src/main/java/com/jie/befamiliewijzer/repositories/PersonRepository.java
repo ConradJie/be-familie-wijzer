53f7a9ce45551f695f2a63cf483c9c909b23a637
conradjie <conrad.jie@gmail.com>
2023-12-08 10:35:20 +0100

    persons without relations or child of

--- src/main/java/com/jie/befamiliewijzer/repositories/PersonRepository.java
+++ src/main/java/com/jie/befamiliewijzer/repositories/PersonRepository.java
@@ -2,6 +2,7 @@ package com.jie.befamiliewijzer.repositories;
 
 import com.jie.befamiliewijzer.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +14,27 @@ public interface PersonRepository extends JpaRepository<Person, Integer> {
     List<Person> findAllByGivenNamesIsContainingIgnoreCaseAndSurnameIsContainingIgnoreCaseOrderByGivenNames(String givenNames,String surname);
     List<Person> findAllByGivenNamesIsContainingIgnoreCaseOrderByGivenNames(String givenNames);
     List<Person> findAllBySurnameContainingIgnoreCaseOrderBySurname(String surname);
+
+    @Query(value = """
+            SELECT
+              id,
+              given_names,
+              surname,
+              sex
+            FROM persons
+            EXCEPT
+            (SELECT p.*
+            FROM persons p
+            JOIN children ON children.child_id = p.id
+
+            UNION
+
+            SELECT p.*
+            FROM persons p
+            JOIN relations r ON r.person_id = p.id
+                             OR r.spouse_id = p.id
+            )
+            """
+            , nativeQuery = true)
+    List<Person> findAllPersonswithoutRelationsOrChildOf();
 }
